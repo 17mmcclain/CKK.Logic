ac8c440cabf9b8a195ecd67b1a7fa333439b8c71
17mmcclain <minji_m@yahoo.com>
2024-01-26 10:14:44 -0700

    Finished first deliverable.

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class Customer
+    {
+        private int _id;
+        private string _name;
+        private string _address;
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public int SetId(int id)
+        {
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string SetName(string name)
+        {
+            _name = name;
+        }
+
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        public string SetAddress(string address)
+        {
+            _address = address;
+        }
+    }
+}
---
+++ CKK.Logic/Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private decimal _price;
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public int SetId(int id)
+        {
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string SetName(string name)
+        {
+            _name = name;
+        }
+
+        public decimal GetPrice()
+        {
+            return _price;
+        }
+
+        public decimal SetPrice(decimal price)
+        {
+            _price = price;
+        }
+    }
+}
