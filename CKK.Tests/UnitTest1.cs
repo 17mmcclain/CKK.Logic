08c41f5df742b2fe5cfad2aeaa1b2e6ca0f4a05d
Minji McClain <minji_m@yahoo.com>
2024-01-31 13:27:01 -0700

    ProjectDeliverable4B complete

---
+++ CKK.Tests/UnitTest1.cs
@@ -0,0 +1,133 @@
+using CKK.Logic.Models;
+
+namespace CKK.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ShouldAddProduct()
+        {
+            //Assemble
+            Customer test1 = new Customer();
+            ShoppingCart item = new ShoppingCart(test1);
+            Product apples = new Product();
+            int expected = 7;
+
+            //Act
+            item.AddProduct(apples, 7);
+            int actual = item.GetProduct(1).GetQuantity();
+
+            //Assert
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldAddProductWrong()
+        {
+
+            //Assemble
+            Customer customer = new Customer();
+            ShoppingCart cart = new ShoppingCart(customer);
+            Product apples = new Product();
+            int failexpected = 5;
+
+            //Act
+            cart.AddProduct(apples, 3);
+            int actual = cart.GetProduct(1).GetQuantity();
+
+            //Assert
+            Assert.Equal(failexpected, actual);
+        }
+
+        [Fact]
+        public void ShouldRemoveProduct()
+        {
+            //Assemble
+            Customer customer = new Customer();
+            ShoppingCart cart = new ShoppingCart(customer);
+            Product apples = new Product();
+            int expected = 3;
+            cart.AddProduct(apples, 7);
+
+            //Act
+            cart.RemoveProduct(apples, 4);
+            int actual = cart.GetProduct(1).GetQuantity();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldFailatRemovingProduct()
+        {
+            //Assemble
+            Customer customer = new Customer();
+            ShoppingCart cart = new ShoppingCart(customer);
+            Product apples = new Product();
+            int expected = 0;
+            cart.AddProduct(apples, 8);
+
+            //Act
+            cart.RemoveProduct(apples, 7);
+            int actual = cart.GetProduct(1).GetQuantity();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldGetTotal()
+        {
+            //Assemble
+            Customer customer = new Customer();
+            ShoppingCart cart = new ShoppingCart(customer);
+
+            Product product1 = new Product();
+            product1.SetPrice(1);
+            cart.AddProduct(product1);
+            
+            Product product2 = new Product();
+            product2.SetPrice(2);
+            cart.AddProduct(product2);
+
+            Product product3 = new Product();
+            product3.SetPrice(3);
+            cart.AddProduct(product3);
+            decimal expected = 6;
+
+            //Act
+            decimal actual = cart.GetTotal();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldFailGetTotal()
+        {
+            //Assemble
+            Customer customer = new Customer();
+            ShoppingCart cart = new ShoppingCart(customer);
+
+            Product product1 = new Product();
+            product1.SetPrice(1);
+            cart.AddProduct(product1);
+
+            Product product2 = new Product();
+            product2.SetPrice(2);
+            cart.AddProduct(product2);
+
+            Product product3 = new Product();
+            product3.SetPrice(3);
+            cart.AddProduct(product3);
+            decimal expected = 8;
+
+            //Act
+            decimal actual = cart.GetTotal();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
